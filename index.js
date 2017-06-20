31dac997fdee4b36a53e0fbd0a186d28ee95edf7
Elliot Brock <beta.redex@gmail.com>
2017-06-20 03:23:36 -0400

    Added cron support

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var Twitter = require('twitter');
 var _ = require('lodash');
 
+var CronJob = require('cron').CronJob;
 var client = new Twitter(config.auth);
 
 try{
@@ -18,23 +19,32 @@ if(config.path[config.path.length] != '/'){
   config.path += '/'
 }
 
-setInterval(function(){
-    fs.readdir(config.path, function(err, items){
-      if(err){return console.error(err);}
-      _.filter(items, function(e){return e.match(/\.(jpg|png)$/)});
-      queue = _.difference(items, posted);
-      if(queue.length > 0){
-        var data = require('fs').readFileSync(config.path + queue[0]);
-        client.post('media/upload', {media: data}, function(err, media, response){
+var post = function(){
+  fs.readdir(config.path, function(err, items){
+    if(err){return console.error(err);}
+    _.filter(items, function(e){return e.match(/\.(jpg|png)$/)});
+    queue = _.difference(items, posted);
+    if(queue.length > 0){
+      var data = require('fs').readFileSync(config.path + queue[0]);
+      client.post('media/upload', {media: data}, function(err, media, response){
+        if(err){return console.error(err)}
+        client.post('statuses/update', {media_ids: media.media_id_string}, function(err, tweet, res){
           if(err){return console.error(err)}
-          client.post('statuses/update', {media_ids: media.media_id_string}, function(err, tweet, res){
-            if(err){return console.error(err)}
-            console.log("Tweeted " + queue[0]);
-            posted.push(queue.unshift());
-            fs.writeFileSync(config.log, JSON.stringify(posted));
-          });
+          console.log("Tweeted " + queue[0]);
+          posted.push(queue.shift());
+          fs.writeFileSync(config.log, JSON.stringify(posted));
         });
-      }
-    });
-  }, config.interval
-);
+      });
+    }
+  });
+}
+
+if(config.cron){
+  var job = new CronJob(config.cron, post, null, true, 'America/New_York');
+}
+else if(config.interval){
+  setInterval(post, config.interval);
+}
+else{
+  post();
+}
